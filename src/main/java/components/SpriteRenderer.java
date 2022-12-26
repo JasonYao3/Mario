d4f6a6948d0d29d2f27281d604cc5cb6636fc55f
Jason Yao <jasonyao105025@gmail.com>
2022-12-26 18:24:14 -0500

    8.Implement the Component Pattern (uses the GameObject as a middleman)

---
+++ src/main/java/components/SpriteRenderer.java
@@ -0,0 +1,19 @@
+package components;
+
+import engine.Component;
+
+public class SpriteRenderer extends Component {
+
+    private boolean firstTime = false;
+    @Override
+    public void start() {
+        System.out.println("I am Starting.");
+    }
+    @Override
+    public void update(float dt) {
+        if (!firstTime) {
+            System.out.println("I am updating.");
+            firstTime = true;
+        }
+    }
+}
