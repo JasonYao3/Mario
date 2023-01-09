ffb42eff097c640df6266aeefdb9102d7f4534f1
Jason Yao <jasonyao105025@gmail.com>
2023-01-09 11:17:16 -0500

    14. Implement Alpha Blending and Z-Indexing for the batched renderer.

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -21,9 +21,9 @@ public class Window {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
-        r = 0.7f;
-        b = 0.2f;
-        g = 0.1f;
+        r = 1;
+        b = 1;
+        g = 1;
         a = 1;
     }
 
@@ -107,6 +107,9 @@ public class Window {
         glfwShowWindow(glfwWindow);
 
         GL.createCapabilities();
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
         Window.changeScene(0);
     }
