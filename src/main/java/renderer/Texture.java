7a0024a46f4afa5825723961858e531983775ac6
Jason Yao <jasonyao105025@gmail.com>
2023-02-02 13:53:53 -0500

    25. Create framebuffer

--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -10,19 +10,36 @@ import static org.lwjgl.stb.STBImage.*;
 
 public class Texture {
     private String filepath;
-    private int textID;
+    private int texID;
     private int width, height;
 
 //    public Texture(String filepath) {
 //
 //    }
 
+    public Texture() {
+        texID = -1;
+        width = -1;
+        height = -1;
+    }
+    public Texture(int width, int height) {
+        this.filepath = "Generated";
+
+        // Generate texture on GPU
+        texID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, texID);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                GL_UNSIGNED_BYTE, 0);
+
+    }
+
     public void init(String filepath) {
         this.filepath = filepath;
 
         // Generate texture on GPU
-        textID = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, textID);
+        texID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, texID);
 
         // Set texture parameters
         // Repeat image in both directions
@@ -58,7 +75,7 @@ public class Texture {
     }
 
     public void bind() {
-        glBindTexture(GL_TEXTURE_2D, textID);
+        glBindTexture(GL_TEXTURE_2D, texID);
     }
 
     public void unbind() {
@@ -74,6 +91,19 @@ public class Texture {
     }
 
     public int getId() {
-        return textID;
+        return texID;
+    }
+
+    public String getFilepath() {
+        return this.filepath;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Texture)) return false;
+        Texture oTex = (Texture)o;
+        return oTex.getWidth() == this.width && oTex.getHeight() == this.height && oTex.getId() == this.texID &&
+                oTex.getFilepath().equals(this.filepath);
     }
 }
