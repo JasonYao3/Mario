f669d817a0fed060a9b59bc94489225439ce9572
Jason Yao <jasonyao105025@gmail.com>
2023-03-05 21:59:09 -0500

    41. Create Scene Panel Drag & Drop (ImGui)

--- src/main/java/editor/SceneHierarchyWindow.java
+++ src/main/java/editor/SceneHierarchyWindow.java
@@ -8,6 +8,8 @@ import imgui.flag.ImGuiTreeNodeFlags;
 import java.util.List;
 
 public class SceneHierarchyWindow {
+
+    private static String payloadDragDropType = "SceneHierarchy";
     public void imgui() {
         ImGui.begin("Scene Hierarchy");
 
@@ -18,16 +20,7 @@ public class SceneHierarchyWindow {
                 continue;
             }
 
-            ImGui.pushID(index);
-            boolean treeNodeOpen = ImGui.treeNodeEx(
-                    obj.name,
-                    ImGuiTreeNodeFlags.DefaultOpen |
-                            ImGuiTreeNodeFlags.FramePadding |
-                            ImGuiTreeNodeFlags.OpenOnArrow |
-                            ImGuiTreeNodeFlags.SpanAvailWidth,
-                    obj.name
-            );
-            ImGui.popID();
+            boolean treeNodeOpen = doTreeNode(obj, index);
 
             if (treeNodeOpen) {
                 ImGui.treePop();
@@ -36,5 +29,37 @@ public class SceneHierarchyWindow {
         }
 
         ImGui.end();
+    }
+
+    private boolean doTreeNode(GameObject obj, int index) {
+        ImGui.pushID(index);
+        boolean treeNodeOpen = ImGui.treeNodeEx(
+                obj.name,
+                ImGuiTreeNodeFlags.DefaultOpen |
+                        ImGuiTreeNodeFlags.FramePadding |
+                        ImGuiTreeNodeFlags.OpenOnArrow |
+                        ImGuiTreeNodeFlags.SpanAvailWidth,
+                obj.name
+        );
+        ImGui.popID();
+
+        if (ImGui.beginDragDropSource()) {
+            ImGui.setDragDropPayloadObject(payloadDragDropType,  obj);
+            ImGui.text(obj.name);
+            ImGui.endDragDropSource();
+        }
+
+        if (ImGui.beginDragDropTarget()) {
+            Object payloadObj = ImGui.acceptDragDropPayloadObject(payloadDragDropType);
+            if (payloadObj != null) {
+                if (payloadObj.getClass().isAssignableFrom(GameObject.class)) {
+                    GameObject playerGameObj = (GameObject) payloadObj;
+                    System.out.println("Payload accepted " + playerGameObj.name );
+                }
+            }
+            ImGui.endDragDropTarget();
+        }
+
+        return treeNodeOpen;
     }
 }
